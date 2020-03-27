7c3d671295338d92a67167b11d4dd26645a37552
RobotSpike <r0b0tsp1k3@gmail.com>
2020-03-27 15:44:47 -0700

    CPU multiprocessing works, GPU to be tested

--- quarantine.py
+++ quarantine.py
@@ -8,6 +8,7 @@ import tempfile
 import time
 from random import shuffle
 import glob
+import shutil
 
 from docking.autodock import runAutodock, prepDPFshell
 from docking.autogrid import runAutogrid
@@ -24,7 +25,8 @@ sentry_errors_log.addHandler(logging.StreamHandler())
 
 # Currently designed to be CPU or GPU centric processing, so you can launch one container of each type.
 # TODO maybe change autodock.py to allow using all CPUs + GPUs
-
+# Issue with multiprocessing and check_call worked around using this approach
+# https://gist.github.com/ownport/63167dbb162f998964f309a5046bef58
 
 #!/usr/bin/env python
 import signal
@@ -64,7 +66,6 @@ which tranche file should be processed.
 devmode = os.getenv('DEBUG')		# if set, enters developer mode (contacts local server
 USERNAME = os.getenv('ME')		# if set, enters developer mode (contacts local server
 
-
 cpu_count = multiprocessing.cpu_count() / 2 # assume hyperthreading so ignore half the logical cores
 gpu_count = 1 # assuming one GPU job at a time, use nvidia-smi to see if you have <100% utilization
 jobs_to_cache = cpu_count # have one job per core available, or 3-5 jobs if GPU running
@@ -76,14 +77,13 @@ def fetchLoop(work_new):
 
 	while True:
 
-		while work_new.qsize() > jobs_to_cache:		# Puts the breaks on execution
-			time.sleep(1)
-
 		trancheID, tranche = client.nextTranche()		# contact server for a tranche assignment
 		TR = TrancheReader(trancheID, tranche, mirror=client.mirror)			# then download and open this tranche for reading
 
 		# inner loop - which ligand models from this tranche file should we execute?
 		while True:
+			while work_new.qsize() > jobs_to_cache:  # Puts the breaks on execution
+				time.sleep(1)
 			# get model number from server
 			ligandNum, receptors = client.nextLigand(trancheID)					# ask server which ligand model number to execute
 			print('Server told us to work on model '+str(ligandNum))
@@ -97,35 +97,99 @@ def fetchLoop(work_new):
 				print('queueing docking algorithm on '+str(receptor))
 
 				# Creating a temporary directory per job
-				dir = tempfile.TemporaryDirectory()
-				work_new.put(dir)
+				dir = tempfile.mkdtemp() # .TemporaryDirectory()
+				work_new.put([dir, receptor, trancheID, ligandNum, client])
 				#dir = os.path.join(os.getcwd(), 'receptors', receptor)
 
 				if not os.path.exists(dir):			# if a new receptor has been deployed, but we don't have it, stop the client and run git-pull
-					raise ValueError("Don't have this receptor definition yet")
+					raise ValueError("Creating temporary file failed")
 					#sys.exit(1)
+
+				# Need to copy receptor file
+				receptor_dir = os.path.join(os.getcwd(), 'receptors', receptor)
+				if not os.path.exists(receptor_dir):  # if a new receptor has been deployed, but we don't have it, stop the client and run git-pull
+					raise ValueError("Don't have this receptor definition yet")
+					# sys.exit(1)
+				print("Copying "+receptor_dir+" to "+dir)
+				try:
+					#shutil.copytree(receptor_dir, dir)
+					shutil.copy(receptor_dir+'/template.gpf', dir)
+					shutil.copy(receptor_dir+'/receptor.pdbqt', dir)
+				except OSError:
+					pass
+				#check_call([ls, dir])
 
 				TR.saveModel(model, outfile=os.path.join(dir, 'ligand.pdbqt'))
 				sys.stdout.flush()	# make sure child processes print
+			time.sleep(0.5) # slow down to help syncing, server
 	return
 
-# Run one of these per physical core
-def cpuLoop(work_new, work_gpu):
-	print("CPU worker started")
-	while True:
+class CpuConsumer(multiprocessing.Process):
+	def __init__(self, work_new, work_gpu):
+		multiprocessing.Process.__init__(self)
+		self.work_new = work_new
+		self.work_gpu = work_gpu
 
-		while work_new.empty() or work_gpu.qsize() > jobs_to_cache:
-			time.sleep(1)
+	# Run one of these per physical core
+	def run(self):
+		proc_name = self.name
+		print("CPU worker started")
+		while True:
+			while self.work_new.empty() or self.work_gpu.qsize() > jobs_to_cache:
+				time.sleep(1)
+			dir, receptor, trancheID, ligandNum, client = self.work_new.get()
+			if dir is POISON_PILL: # poison pill to exit
+				print('CPU {}: CPU exiting'.format(proc_name))
+				self.work_new.task_done()
+				break
+			print('CPU {}: {}'.format(proc_name, dir))
+			start = time.time()
+			# Tasks that run on the CPU
+			runAutogrid(cwd=dir)
+			prepDPFshell(cwd=dir)
 
-		dir = work_new.get()
-		start = time.time()
-		# Tasks that run on the CPU
-		runAutogrid(cwd=dir)
-		prepDPFshell(cwd=dir)
+			# Tasks that may run on the GPU
+			if not isGPU():
+				print('running docking algorithm on CPU in '+str(dir))
+				results, logFile = runAutodock(cwd=dir)
+				end = time.time()
+				results['time'] = end - start
+				results['receptor'] = receptor
+				results['tranche'] = trancheID
+				results['ligand'] = ligandNum
 
-		# Tasks that may run on the GPU
-		if not isGPU():
-			print('running docking algorithm on CPU in '+str(dir))
+				# FIXME - different autodock versions have different logfile formats - some don't export ligand name
+
+				client.reportResults(results, logFile)
+				shutil.rmtree(dir) # hack to remove temporaryDirectory w/o context (https://stackoverflow.com/questions/6884991/how-to-delete-a-directory-created-with-tempfile-mkdtemp)
+			else:
+				self.work_gpu.put(dir)	# hand work over to GPU
+			self.work_new.task_done()
+			sys.stdout.flush()
+			print('CPU work_new='+str(self.work_new.qsize())+' GPU work_gpu='+str(self.work_gpu.qsize()))
+		return
+
+# Only run if GPU version and probably only use one
+class GpuConsumer(multiprocessing.Process):
+	def __init__(self, work_gpu):
+		multiprocessing.Process.__init__(self)
+		self.work_gpu = work_gpu
+
+	# Run one of these per physical core
+	def run(self):
+		proc_name = self.name
+		print("GPU worker started")
+		while True:
+			while self.work_gpu.empty():
+				time.sleep(0.2)
+			dir, receptor, trancheID, ligandNum, client = work_gpu.get()
+			if dir is POISON_PILL: # poison pill to exit
+				print('GPU {}: GPU exiting'.format(proc_name))
+				self.work_gpu.task_done()
+				break
+			print('GPU {}: {}'.format(proc_name, dir))
+			start = time.time()
+			print('running docking algorithm on GPU in '+str(dir))
 			results, logFile = runAutodock(cwd=dir)
 			end = time.time()
 			results['time'] = end - start
@@ -136,72 +200,54 @@ def cpuLoop(work_new, work_gpu):
 			# FIXME - different autodock versions have different logfile formats - some don't export ligand name
 
 			client.reportResults(results, logFile)
-		else:
-			work_gpu.put(dir)	# hand work over to GPU
-		sys.stdout.flush()
-	return
+			shutil.rmtree(dir)
 
-# Only run if GPU version and probably only use one
-def gpuLoop(work_gpu):
-	print("GPU worker started")
-	while True:
-		while work_gpu.empty():
-			time.sleep(0.2)
-
-		dir = work_gpu.get()
-
-		start = time.time()
-		print('running docking algorithm on GPU in '+str(dir))
-		results, logFile = runAutodock(cwd=dir)
-		end = time.time()
-		results['time'] = end - start
-		results['receptor'] = receptor
-		results['tranche'] = trancheID
-		results['ligand'] = ligandNum
-
-		# FIXME - different autodock versions have different logfile formats - some don't export ligand name
-
-		client.reportResults(results, logFile)
-		sys.stdout.flush()
-	return
+			self.work_gpu.task_done()
+			sys.stdout.flush()
+		return
 
 def dispatchCenter():
-	# create a manager - it lets us share native Python object types like
-	# lists and dictionaries without worrying about synchronization -
-	# the manager will take care of it
-	manager = multiprocessing.Manager()
-
-	# now using the manager, create our shared data structures
-	work_new = manager.Queue()
-	work_gpu = manager.Queue()
+	work_new = multiprocessing.JoinableQueue()
+	work_gpu = multiprocessing.JoinableQueue()
 
 	if not isGPU():
-		worker_count = 1 + cpu_count
+		worker_count = cpu_count
 	else: # is GPU
-		worker_count = 2 # no need for overkill, one fetch and one cpu for unpack
+		worker_count = 1 # no need for overkill, one cpu for unpack
 		jobs_to_cache = 3 # probably enough buffering to keep GPU fed, else up to 5
 	print("CPU workers count = "+str(cpu_count))
 
 	# Loops getting stuck, trying this approach https://stackoverflow.com/questions/29571671/basic-multiprocessing-with-while-loop
 	if isGPU():
-		print("Starting GPU workers")
-		pool_gpu = multiprocessing.Pool(processes=1)
-		gpu_res = pool_gpu.apply_async(func=gpuLoop, args=(work_gpu))
-		print("Starting CPU workers")
-		pool_cpu = multiprocessing.Pool(processes=1)
-		cpu_res = pool_cpu.apply_async(func=cpuLoop, args=(work_new, work_gpu))
+		cpu_consumers = [
+			CpuConsumer(work_new, work_gpu)
+			for i in range(worker_count)
+		]
+		for cw in cpu_consumers:
+			cw.start()
+		gpu_consumers = [
+			GpuConsumer(work_gpu)
+			for i in range(gpu_count)
+		]
+		for gw in gpu_consumers:
+			gw.start()
 	else:
-		print("Starting CPU workers")
-		pool_cpu = multiprocessing.Pool(processes=cpu_count)
-		cpu_res = pool_cpu.apply_async(func=cpuLoop, args=(work_new, work_gpu))
+		cpu_consumers = [
+			CpuConsumer(work_new, work_gpu)
+			for i in range(worker_count)
+		]
+		for cw in cpu_consumers:
+			cw.start()
 
 	print("Starting fetching process")
 	fetchLoop(work_new)
-	pool_cpu.close()
-	pool_cpu.join()
+	for i in range(worker_count):
+		work_new.put(POISON_PILL)
+	work_new.join()
 	if isGPU():
-		pool_gpu.close()
-		pool_gpu.join()
+		for i in range(gpu_count):
+			work_gpu.put(POISON_PILL)
+		work_gpu.join()
 
 	return
 
