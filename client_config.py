b7721631fd00e48a1dd1c5e4086d698477cc7dc2
cosmo <clinton.mielke@gmail.com>
2020-04-02 22:23:30 -0700

    adding spec file before I accidentally delete it again

--- client_config.py
+++ client_config.py
@@ -3,8 +3,8 @@ class ClientConfig(object):
     APP_NAME = 'QuarantineAtHome'
     COMPANY_NAME = 'Project InfinoMe'
     UPDATE_URLS = [
-        'https://cjmielke.github.io/quarantine-files/updates/'
-        'https://quarantine.infino.me/static/updates/',
+        'https://cjmielke.github.io/quarantine-files/updates/',
+        #'https://quarantine.infino.me/static/updates/'
     ]
     MAX_DOWNLOAD_RETRIES = 3
 
